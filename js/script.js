a881573989ffaf8a7401258c75da325b3ce020db
Kaeneth Dayao <kendayao@gmail.com>
2020-08-21 11:11:16 -0700

    added response data info

--- js/script.js
+++ js/script.js
@@ -19,6 +19,19 @@ function apiCall(){
         $(".find-input").val("")
         console.log(data)
         
+        const resImage=data.Poster
+        const resTitle=data.Title
+        const resYear=data.Year
+        const resDirector=data.Director
+        const resActors=data.Actors
+        const resPlot=data.Plot
+        const resRated=data.Rated
+        const resGenre=data.Genre
+        const resRuntime=data.Runtime
+        const resCountry=data.Country
+        const resProduction=data.Production
+        const resAwards=data.Awards
+        
 
         var divContainer=$("<div>")
         divContainer.attr("class","container")
@@ -33,7 +46,7 @@ function apiCall(){
         divCol1.attr("class","col-md-6")
         $(divRow).append(divCol1)
         var imgEl=$("<img>")
-        imgEl.attr({src: data.Poster,
+        imgEl.attr({src: resImage,
         class: "movie-image",
         alt: "movie-poster"})
         $(divCol1).append(imgEl)
@@ -43,9 +56,36 @@ function apiCall(){
 
         var divCol2=$("<div>")
         divCol2.attr("class","col-md-6")
-        var h3El=$("<h3>")
-        h3El.text(data.Title)
-        $(divCol2).append(h3El)
+        var h3TitleEl=$("<h3>")
+        h3TitleEl.text(resTitle+" "+"("+resYear+")") 
+        $(divCol2).append(h3TitleEl)
+        var pDirectorEl=$("<p>")
+        pDirectorEl.text("Director "+resDirector)
+        $(divCol2).append(pDirectorEl)
+        var pActorsEl=$("<p>")
+        pActorsEl.text("Actors "+resActors)
+        $(divCol2).append(pActorsEl)
+        var pPlotEl=$("<p>")
+        pPlotEl.text(resPlot)
+        $(divCol2).append(pPlotEl)
+        var pRatedEl=$("<p>")
+        pRatedEl.text("Rated"+resRated)
+        $(divCol2).append(pRatedEl)
+        var pGenreEl=$("<p>")
+        pGenreEl.text(resGenre)
+        $(divCol2).append(pGenreEl)
+        var pRuntimeEl=$("<p>")
+        pRuntimeEl.text(resRuntime)
+        $(divCol2).append(pRuntimeEl)
+        var pCountryEl=$("<p>")
+        pCountryEl.text(resCountry)
+        $(divCol2).append(pCountryEl)
+        var pProductionEl=$("<p>")
+        pProductionEl.text(resProduction)
+        $(divCol2).append(pProductionEl)
+        var pAwardsEl=$("<p>")
+        pAwardsEl.text(resAwards)
+        $(divCol2).append(pAwardsEl)
         divRow.append(divCol2)
         
 
