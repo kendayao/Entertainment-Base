aeb9dc6b34e71e1c48937b14350cf755d1e143d4
Kaeneth Dayao <kendayao@gmail.com>
2020-08-21 12:01:42 -0700

    styled right column

--- js/script.js
+++ js/script.js
@@ -33,17 +33,17 @@ function apiCall(){
         const resAwards=data.Awards
         
 
-        var divContainer=$("<div>")
-        divContainer.attr("class","container")
-        $(".content").append(divContainer)
+        // var divContainer=$("<div>")
+        // divContainer.attr("class","container")
+        // $(".content").append(divContainer)
 
 
 
         var divRow=$("<div>")
         divRow.attr("class","row")
-        $(divContainer).append(divRow)
+        $(".content").append(divRow)
         var divCol1=$("<div>")
-        divCol1.attr("class","col-md-6")
+        divCol1.attr("class","col-md-6 search-image")
         $(divRow).append(divCol1)
         var imgEl=$("<img>")
         imgEl.attr({src: resImage,
@@ -55,37 +55,79 @@ function apiCall(){
 
 
         var divCol2=$("<div>")
-        divCol2.attr("class","col-md-6")
+        divCol2.attr("class","col-md-6 search-details")
+
         var h3TitleEl=$("<h3>")
         h3TitleEl.text(resTitle+" "+"("+resYear+")") 
         $(divCol2).append(h3TitleEl)
+
+        var pDirectorLabelEl=$("<p>")
+        pDirectorLabelEl.text("Director")
+        pDirectorLabelEl.attr("class", "response-label")
+        $(divCol2).append(pDirectorLabelEl)
         var pDirectorEl=$("<p>")
-        pDirectorEl.text("Director "+resDirector)
+        pDirectorEl.text(resDirector)
         $(divCol2).append(pDirectorEl)
+
+        var pActorsLabelEl=$("<p>")
+        pActorsLabelEl.text("Actors")
+        pActorsLabelEl.attr("class", "response-label")
+        $(divCol2).append(pActorsLabelEl)
         var pActorsEl=$("<p>")
-        pActorsEl.text("Actors "+resActors)
+        pActorsEl.text(resActors)
         $(divCol2).append(pActorsEl)
+
+       
+        var pPlotLabelEl=$("<p>")
+        pPlotLabelEl.text("Plot")
+        pPlotLabelEl.attr("class", "response-label")
+        $(divCol2).append(pPlotLabelEl)   
         var pPlotEl=$("<p>")
         pPlotEl.text(resPlot)
         $(divCol2).append(pPlotEl)
+
+        var pRatedLabelEl=$("<p>")
+        pRatedLabelEl.text("Rated")
+        pRatedLabelEl.attr("class", "response-label")
+        $(divCol2).append(pRatedLabelEl) 
         var pRatedEl=$("<p>")
-        pRatedEl.text("Rated"+resRated)
+        pRatedEl.text(resRated)
         $(divCol2).append(pRatedEl)
+
+        var pGenreLabelEl=$("<p>")
+        pGenreLabelEl.text("Genre")
+        pGenreLabelEl.attr("class", "response-label")
+        $(divCol2).append(pGenreLabelEl) 
         var pGenreEl=$("<p>")
         pGenreEl.text(resGenre)
         $(divCol2).append(pGenreEl)
+
+
+        var pRuntimeLabelEl=$("<p>")
+        pRuntimeLabelEl.text("Runtime")
+        pRuntimeLabelEl.attr("class", "response-label")
+        $(divCol2).append(pRuntimeLabelEl)
         var pRuntimeEl=$("<p>")
         pRuntimeEl.text(resRuntime)
         $(divCol2).append(pRuntimeEl)
+
+        var pCountryLabelEl=$("<p>")
+        pCountryLabelEl.text("Country")
+        pCountryLabelEl.attr("class", "response-label")
+        $(divCol2).append(pCountryLabelEl)
         var pCountryEl=$("<p>")
         pCountryEl.text(resCountry)
         $(divCol2).append(pCountryEl)
-        var pProductionEl=$("<p>")
-        pProductionEl.text(resProduction)
-        $(divCol2).append(pProductionEl)
+
+        var pAwardsLabelEl=$("<p>")
+        pAwardsLabelEl.text("Awards")
+        pAwardsLabelEl.attr("class", "response-label")
+        $(divCol2).append(pAwardsLabelEl)
         var pAwardsEl=$("<p>")
         pAwardsEl.text(resAwards)
         $(divCol2).append(pAwardsEl)
+
+
         divRow.append(divCol2)
         
 
